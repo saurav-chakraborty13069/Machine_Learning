c90e431ee41c39d5ba129615b1069d800c315ab3
EC2 Default User <ec2-user@ip-172-31-40-235.ec2.internal>
2020-03-23 12:53:19 +0000

    changed name of folder

---
+++ ML_Projects/ML_Models/Cats_and_Dogs_CNN/cnn.py
@@ -0,0 +1,160 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  6 18:39:48 2019
+
+@author: saurav.a.chakraborty
+"""
+
+#Kaggle Dataset CNN - Cats and Dogs
+import pickle
+from sklearn.externals import joblib
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dense
+from keras.wrappers.scikit_learn import KerasClassifier
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from keras.preprocessing import image
+'''
+# Initialising the CNN
+classifier = Sequential()
+# Step 1 - Convolution
+classifier.add(Conv2D(32, (3, 3), input_shape = (64, 64, 3), activation = 'relu'))
+# Step 2 - Pooling
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+# Adding a second convolutional layer
+classifier.add(Conv2D(32, (3, 3), activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+# Step 3 - Flattening
+classifier.add(Flatten())
+# Step 4 - Full connection
+classifier.add(Dense(units = 128, activation = 'relu'))
+classifier.add(Dense(units = 1, activation = 'sigmoid'))
+# Compiling the CNN
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+
+#image preparation
+train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+training_set = train_datagen.flow_from_directory('dataset/training_set',
+                                                target_size=(64, 64),
+                                                batch_size=32,
+                                                class_mode='binary')
+
+test_set = test_datagen.flow_from_directory('dataset/test_set',
+                                            target_size=(64, 64),
+                                            batch_size=32,
+                                            class_mode='binary')
+
+classifier.fit_generator(training_set,
+                        steps_per_epoch=5148,
+                        epochs=50,
+                        validation_data=test_set,
+                        validation_steps=2023)
+'''
+#==============================================================================
+
+target_size = (256, 256)
+batch_size = 32
+epochs = 90
+input_shape = target_size + (3,)
+
+classifier = Sequential()
+classifier.add(Conv2D(32, (3, 3), input_shape = (256, 256, 3), activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+classifier.add(Conv2D(64, (3, 3), activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+classifier.add(Conv2D(128, (3, 3), activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+classifier.add(Conv2D(256, (3, 3), activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+classifier.add(Flatten())
+classifier.add(Dropout(0.5))
+classifier.add(Dense(units = 512, activation = 'relu'))
+classifier.add(Dropout(0.5))
+classifier.add(Dense(units = 512, activation = 'relu'))
+classifier.add(Dropout(0.5))
+classifier.add(Dense(units = 1, activation = 'sigmoid'))
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+#model summary
+classifier.summary()
+
+#image preprocesing and fitting
+train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+training_set = train_datagen.flow_from_directory('dataset/training_set',
+                                                target_size=target_size,
+                                                batch_size=batch_size,
+                                                class_mode='binary')
+
+test_set = test_datagen.flow_from_directory('dataset/test_set',
+                                            target_size=target_size,
+                                            batch_size=batch_size,
+                                            class_mode='binary')
+
+classifier.fit_generator(training_set,
+                        steps_per_epoch=5148,
+                        epochs=epochs,
+                        validation_data=test_set,
+                        validation_steps=2023)
+
+
+#==========================================================================
+
+#predicting new image
+test_image = image.load_img('dataset/single_prediction/cat_or_dog_3.jpg', target_size = (64,64))
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image, axis = 0)
+result = classifier.predict(test_image)
+if result[0][0] == 1:
+    prediction = 'dog'
+else:
+    prediction = 'cat'
+
+#==========================================================================
+'''
+classifier.save('dataset//model.h5')
+joblib.dump(classifier, 'dataset//joblib_model.pkl') 
+pickle.dump(classifier, open('pickle_model.pkl','wb'))
+'''
+'''
+import h5py
+filename = 'file.hdf5'
+f = h5py.File(filename, 'r')
+
+# List all groups
+print("Keys: %s" % f.keys())
+a_group_key = list(f.keys())[0]
+
+# Get the data
+data = list(f[a_group_key])
+'''
+===========================================================================
+
+
+
+
+
+
+
+
